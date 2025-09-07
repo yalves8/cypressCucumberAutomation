bf8cc5c3cf25fd1adeb466be8dc5b032755b23d3
Yasmin Souza <yasminleticia_@hotmail.com>
2025-09-06 21:04:15 -0300

    config cypress

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: "**/*.feature",
+    async setupNodeEvents(on, config) {
+      // Para suportar arquivos .feature
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
+    },
+    baseUrl: "https://demoqa.com",
+  },
+});
